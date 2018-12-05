e6ee0bd8e74abbc57c31e980227c8028f29856ae
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-12-05 13:50:19 -0800

    Update completed

--- Poor-fund-App/src/app/nav/nav.component.ts
+++ Poor-fund-App/src/app/nav/nav.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  constructor() { }
+  authenticatedName: any;
+  constructor(public af: AngularFireAuth, private router: Router) {
+   this.af.authState.subscribe(auth => {
+     if (!auth.displayName) {
+       this.authenticatedName = auth.email;
+     } else {
+       this.authenticatedName = auth.displayName;
+     }
+   });
+
+    }
+    // FOR NAV COLLAPSE
   isCollapsed = true;
+  // LOGOUT
+  logout() {
+    this.af.auth.signOut();
+    this.authenticatedName = null;
+     this.router.navigateByUrl('/login');
+  }
   ngOnInit() {
   }
 
